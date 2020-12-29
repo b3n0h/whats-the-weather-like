f0513e0f551757849ec65fa9f7600fb4ff26db2d
b3n0h <b3njaminoh@gmail.com>
2020-12-29 12:36:30 -0800

    data output successfull

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const key = config.key
 const cityInput = document.querySelector('#cityInput')
+let showWeather = document.querySelector('#displayWeather')
 
 cityInput.addEventListener('change', () => {
   const city = cityInput.value
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${key}`
   fetch(url)
     .then(r => r.json())
-    .then(data => console.log(data))
+    .then(data => displayWeather(data))
     .catch(e => console.log(e))
 })
+
+function displayWeather(data) {
+  console.log(data)
+  console.log(data.weather[0])
+  let cityName = data.name
+  let cityTemp = data.main.temp.toString().slice(0, 2)
+  showWeather.innerHTML = `
+  The temperature in ${cityName} is ${cityTemp}°F <br />
+  `
+}
